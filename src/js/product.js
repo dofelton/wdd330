71d2383a1fe8c5c5a7f41ca20811b0d2e281e9c1
JosueSinaca <sinaca56@gmail.com>
2024-06-21 22:53:41 -0600

    correct some error we are getting in our code

--- src/js/product.js
+++ src/js/product.js
@@ -9,7 +9,7 @@
 // async function addToCartHandler(e) {
 //   const product = await dataSource.findProductById(e.target.dataset.id);
 //   addProductToCart(product);
-  
+
 // }
 
 // // add listener to Add to Cart button
@@ -17,13 +17,12 @@
 //   .getElementById("addToCart")
 //   .addEventListener("click", addToCartHandler);
 
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParams } from "./utils.mjs";
 
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { getParams } from './utils.mjs';
-
-const productId = getParams('product');
-const dataSource = new ProductData('tents');
+const productId = getParams("product");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();

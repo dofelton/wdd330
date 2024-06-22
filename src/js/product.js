285cedc183c307a360cf8253a82af868428665cb
Don Felton <dofelton@gmail.com>
2024-06-22 09:26:37 -0500

    sync

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,7 @@
 // import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
 // import ProductData from "./ProductData.mjs";
 // const dataSource = new ProductData("tents");
-// const productId = getParam('product');
+// const productId = getParam("product");
 
 // console.log(dataSource.findProductById(productId));
 
@@ -18,12 +18,12 @@
 //   .addEventListener("click", addToCartHandler);
 
 
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { getParams } from './utils.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParams } from "./utils.mjs";
 
-const productId = getParams('product');
-const dataSource = new ProductData('tents');
+const productId = getParams("product");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();

a5d7616f747f599bb870164d0a545f11973e9ef5
Don Felton <dofelton@gmail.com>
2024-06-28 11:54:56 -0500

    end of week3

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -56,9 +56,9 @@ async function loadTemplate(path) {
 
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("../partials/header.html");
-  const headerElement = document.querySelector("#header");
+  const headerElement = document.querySelector("#main-header");
   const footerTemplate = await loadTemplate("../partials/footer.html");
-  const footerElement = document.querySelector("#footer");
+  const footerElement = document.querySelector("#main-footer");
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
